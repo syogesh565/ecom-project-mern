81fec29b2f19319f6bf82250eb889b02f416c40c
syogesh565 <syogesh565@gmail.com>
2024-01-25 22:44:39 +0530

    all set

--- src/components/stripeConfig.js
+++
@@ -1,6 +0,0 @@
-// // stripeConfig.js
-// import { loadStripe } from '@stripe/stripe-js';
-
-// const stripePromise = loadStripe('pk_test_51OWeBESCz0YwIkJAl9tBnlEIgwvktLLpM354DwSZjiZiziZwXXIywTIDhdwPc9PqY5no65LBrY5dWl4sBn0Lddoo00pp9TrLUN');
-
-// export default stripePromise;
