727fcaa69e02b59bd58272811bb647b88974a34a
syogesh565 <syogesh565@gmail.com>
2024-09-04 15:55:58 +0530

    100 bar commit

--- Node/routes/UserRoutes.js
+++ Node/routes/UserRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/userController');
+const userController = require('../controllers/UserController.js');
 const orderService = require('../services/orderService');
 
 
