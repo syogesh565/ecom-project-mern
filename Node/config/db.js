3d2ff818954cd12dd84dfe5ed800843a1f11d5cc
syogesh565 <syogesh565@gmail.com>
2024-09-05 12:49:58 +0530

    all good

--- Node/config/db.js
+++ Node/config/db.js
@@ -8,11 +8,32 @@
 // module.exports = sequelize;
 
 
+// const Sequelize = require('sequelize');
+
+// const sequelize = new Sequelize('yogesh_db', 'postgres', 'yogesh', {
+//   host: 'localhost',
+//   dialect: 'postgres',
+// });
+
+// module.exports = sequelize
+
+
+require('dotenv').config();
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('yogesh_db', 'postgres', 'yogesh', {
-  host: 'localhost',
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
+  dialectOptions: {
+    // Disable SSL for local development
+    ssl: false
+  },
+  // Add logging to see the SQL queries
+  logging: console.log
 });
 
-module.exports = sequelize
+// Test the connection
+sequelize.authenticate()
+  .then(() => console.log('Connection has been established successfully.'))
+  .catch(err => console.error('Unable to connect to the database:', err));
+
+module.exports = sequelize; 
