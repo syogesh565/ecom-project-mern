4403c73471f7bd99017a83d2314248facb4e98c4
syogesh565 <syogesh565@gmail.com>
2024-09-06 22:47:19 +0530

    server db

--- React/src/components/config.js
+++ React/src/components/config.js
@@ -1,4 +1,4 @@
 // config.js
-const BASE_URL = 'http://localhost:3000';
-// const BASE_URL = 'https://ecom-project-mern-18.onrender.com';
+// const BASE_URL = 'http://localhost:3000';
+const BASE_URL = 'https://ecom-project-mern-18.onrender.com';
 export default BASE_URL;
