1400ee5433c114aa2c7d340151e6bdee9fbb6a2f
syogesh565 <syogesh565@gmail.com>
2024-09-07 10:48:23 +0530

    hii

--- Node/config/db.js
+++ Node/config/db.js
@@ -18,28 +18,28 @@
 // // module.exports = sequelize
 
 
-require('dotenv').config();
-const Sequelize = require('sequelize');
+// require('dotenv').config();
+// const Sequelize = require('sequelize');
 
-let sequelize;
+// let sequelize;
 
-if (process.env.NODE_ENV === 'production') {
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
-  });
-} else {
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres'
-  });
-}
+// if (process.env.NODE_ENV === 'production') {
+//   sequelize = new Sequelize(process.env.DATABASE_URL, {
+//     dialect: 'postgres',
+//     dialectOptions: {
+//       ssl: {
+//         require: true,
+//         rejectUnauthorized: false
+//       }
+//     }
+//   });
+// } else {
+//   sequelize = new Sequelize(process.env.DATABASE_URL, {
+//     dialect: 'postgres'
+//   });
+// }
 
-module.exports = sequelize;
+// module.exports = sequelize;
 
 
 // require('dotenv').config();
@@ -84,3 +84,43 @@ module.exports = sequelize;
 //   .catch(err => console.error('Unable to connect to the database:', err));
 
 // module.exports = sequelize;
+
+
+require('dotenv').config();
+const Sequelize = require('sequelize');
+
+let sequelize;
+
+if (process.env.NODE_ENV === 'production') {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: process.env.NODE_ENV === 'production' ? { require: true, rejectUnauthorized: false } : false
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  });
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  });
+}
+
+// Test the connection
+sequelize.authenticate()
+  .then(() => console.log('Database connection has been established successfully.'))
+  .catch(err => console.error('Unable to connect to the database:', err.message));
+
+module.exports = sequelize;
