b74b965493faec57a17c4d697eed9a184dd7aef8
syogesh565 <syogesh565@gmail.com>
2024-09-07 14:30:29 +0530

    new

--- Node/index.js
+++ Node/index.js
@@ -21,7 +21,7 @@ app.use(express.static('public'));
 app.use(express.json());
 //app.use(cors())
 app.use(cors({
-  origin : "*",
+  origin : "",
   methods : ["GET","POST","PUT","PATCH","DELETE"],
   credentials : true
 })); // Enable CORS for al
