34598686de735730e130830775802ad208797d16
syogesh565 <syogesh565@gmail.com>
2024-09-08 12:40:32 +0530

    renderrrrrrrrrrr

--- React/src/components/config.js
+++ React/src/components/config.js
@@ -1,4 +1,4 @@
 // config.js
 // const BASE_URL = 'http://localhost:3000';
-const BASE_URL = 'https://mern-3fk5.onrender.com/';
+const BASE_URL = 'https://mern-3fk5.onrender.com';
 export default BASE_URL;
